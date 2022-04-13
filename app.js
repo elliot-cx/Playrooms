b5a5c6b72649b64171fc6e96316e2e8827382168
elliot <elliot.casna@gmail.com>
2022-04-13 23:14:21 +0200

    remove hardware testing

--- app.js
+++ app.js
@@ -14,27 +14,6 @@ app.use(express.json());
 
 app.set('view engine','ejs');
 
-// test
-var os = require('os');
-
-var cpus = os.cpus();
-for(var i = 0, len = cpus.length; i < len; i++) {
-    console.log("CPU %s:", i);
-    var cpu = cpus[i], total = 0;
-
-    for(var type in cpu.times) {
-        total += cpu.times[type];
-    }
-
-    for(type in cpu.times) {
-        console.log("\t", type, Math.round(100 * cpu.times[type] / total));
-    }
-}
-console.log(os.cpus());
-console.log(os.totalmem());
-console.log(os.freemem())
-
-
 // Routing
 
 app.get('/',(req,res) => {
